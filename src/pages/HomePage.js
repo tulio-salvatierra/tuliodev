ccc5a9bd18118aeb85d6b80b54fa1d9a04719c61
Tulio Salvatierra <salvacorp@gmail.com>
2023-09-19 07:09:09 -0500

    Cleaned hompegae code

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Hero from "../components/Hero";
-import Header from "../components/header";
-import Footer from "../components/Footer";
 
 function HomePage() {
   return (
