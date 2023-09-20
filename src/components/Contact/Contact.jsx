580d968724cc57925dab5a2430f66e4030c3a351
Tulio Salvatierra <salvacorp@gmail.com>
2023-09-20 17:30:10 -0500

    configured .env

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -47,7 +47,7 @@ export default function Component() {
         process.env.REACT_APP_SERVICE_ID,
         process.env.REACT_APP_TEMPLATE_ID,
         formRef.current,
-        process.env.REACT_APP_USER_ID
+        process.env.REACT_APP_PUBLIC_KEY
       )
       .then(
         function (response) {
