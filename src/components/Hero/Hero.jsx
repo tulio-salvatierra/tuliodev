af211a10bfe24a8739762ee9acd2e8cf4bfc5854
Tulio Salvatierra <salvacorp@gmail.com>
2023-09-21 21:26:17 -0500

    added animation for Homepage

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Hero.css";
 import hero from "../../assets/images/Untitled design (7).png";
+import { gsap } from "gsap";
 
 function Hero() {
+  const heroRef = useRef(null);
+  useEffect(() => {
+    gsap.set(heroRef.current, {
+      yPercent: -100,
+      opacity: 0,
+    });
+    gsap.to(heroRef.current, {
+      duration: 2,
+      delay: 1,
+      yPercent: 0,
+      opacity: 1,
+      ease: "ease-in",
+    });
+
+    console.log(heroRef);
+  }, []);
   return (
     <div className="hero-container">
-      <div className="imageContainer">
+      <div ref={heroRef} className="imageContainer">
         <img
           src={hero}
           alt="hero"
