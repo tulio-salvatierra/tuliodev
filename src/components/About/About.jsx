4aaeedf72f3bc36ff50afc15cf360d7d0b863005
Tulio Salvatierra <salvacorp@gmail.com>
2023-10-30 05:38:57 -0500

    improved About section layout

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "./About.css";
 import { Box, Grid, Typography } from "@mui/material";
-import { Button } from "reactstrap";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function About() {
   return (
     <section className="container">
       <Grid container spacing={2} marginBottom={10}>
-        <Grid item smdown={12} p={4}>
+        <Grid
+          item
+          smdown={12}
+          p={4}
+          sx={{ marginLeft: "auto", marginRight: "auto" }}
+        >
           <Typography
             variant="h1"
             className="AboutTitle"
-            sx={{ marginTop: 6, marginRight: "auto" }}
+            sx={{ marginTop: 6, marginRight: "auto", marginLeft: "auto" }}
             fontFamily={"'Montserrat', sans-serif"}
             fontWeight={700}
           >
@@ -19,12 +25,17 @@ function About() {
           </Typography>
         </Grid>
         <Grid item smdown={12} smup={8} mt={2}>
+          <Box className="icon-container">
+            {" "}
+            <LinkedInIcon sx={{ fontSize: 50, margin: "auto" }} />
+            <GitHubIcon sx={{ fontSize: 50, margin: "auto" }} />
+          </Box>
           <Typography
             className="About-text"
             component="p"
             sx={{
               margin: "auto",
-              fontSize: "2rem",
+              fontSize: "1.75rem",
               fontWeight: "400",
               textAlign: "justify",
             }}
@@ -34,16 +45,11 @@ function About() {
             background, including CDL trucking and Financial analysis roles. I
             do have a soft spot for music, being a drummer I''ve fortunate to be
             in many bands, I love live music and being creative, which is why I
-            pursued front end development. Originally from Venezuela, located in
-            Chicago, Illinois. Find me on social media or contact me for more
-            information.
+            pursued front end development. Currently looking for a position as a
+            front end developer.
           </Typography>
-          <Box className="pic-container">
-            {" "}
-            <Button size="lg" color="primary" href="/contact">
-              Contact me
-            </Button>
-          </Box>
+
+          <Box className="stack-icons"></Box>
         </Grid>
       </Grid>
     </section>
