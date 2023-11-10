14ed356ae192669f2d395e145463dc2f6222561f
Tulio Salvatierra <salvacorp@gmail.com>
2023-11-10 15:20:41 -0600

    Refactor code for improved readability and performance.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,39 +1,45 @@
 import React from "react";
 import "./Footer.css";
-import { Button } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 function Footer() {
-  const handleClick = () => {
-    window.open("../../public/TulioSalvatierra_CV.pdf");
-  };
   return (
-    <footer className="footer-container">
-      <h4>Connect with me!</h4>
-      <h5>Email: salvacorp@gmail.com</h5>
-      <Button onClick={handleClick}>Download My Resume</Button>
-
-      <IconButton
-        href="https://www.linkedin.com/in/tulio-salvatierra/"
-        target="_blank"
-        rel="noopener noreferrer"
-        size="50px"
-        color="primary"
-      >
-        <LinkedInIcon />
-      </IconButton>
-      <IconButton
-        href="https://github.com/T00lio"
-        target="_blank"
-        rel="noopener noreferrer"
-        size="50px"
-        color="primary"
-      >
-        <GitHubIcon />
-      </IconButton>
-    </footer>
+    <section className="footer-container row">
+      <div className="col">
+        <h4>Connect with me!</h4>
+        <h5>Email: salvacorp@gmail.com</h5>
+        <a
+          href={`${process.env.PUBLIC_URL}/resume.pdf`}
+          target="_blank"
+          rel="noopener noreferrer"
+          download="resume.pdf"
+        >
+          Download My Resume
+        </a>
+      </div>
+      <div className="col">
+        <IconButton
+          href="https://www.linkedin.com/in/tulio-salvatierra/"
+          target="_blank"
+          rel="noopener noreferrer"
+          size="50px"
+          color="primary"
+        >
+          <LinkedInIcon />
+        </IconButton>
+        <IconButton
+          href="https://github.com/T00lio"
+          target="_blank"
+          rel="noopener noreferrer"
+          size="50px"
+          color="primary"
+        >
+          <GitHubIcon />
+        </IconButton>
+      </div>
+    </section>
   );
 }
 
