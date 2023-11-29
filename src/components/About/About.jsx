0cdd16ceaec5bc576a1bf4aaec50ca661e60f2ba
tuliosalvatierra <salvacorp@gmail.com>
2023-11-28 21:07:58 -0600

    Update font size and line height in About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,13 +15,11 @@ function About() {
           Frontend Developer with a background in Finance roles and freight
           management with a passion for solving problems and creating solutions.
           I have a strong interest in the latest technologies and frameworks and
-          I am always looking to learn new skills and improve my knowledge. I am
-          a team player and I am always looking to help others and share my
-          knowledge. Currently looking for a role as a Frontend Developer &
-          learn CRUD operations to become a Full Stack Developer. Also I play
-          drums 🥁
+          I am always looking to learn new skills and improve my knowledge.
+          Currently learning backend and databases to become a full stack
+          developer.
         </h3>
-        <br />
+
         <Button
           className="button"
           href={RESUME}
