65facdd4882d4a8bf7e3f191b4811a254c43ea02
tuliosalvatierra <salvacorp@gmail.com>
2024-01-31 12:55:21 -0600

    Add Quiz App image and description

--- src/components/Projects/ProjectsComp.jsx
+++ src/components/Projects/ProjectsComp.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Projects.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Ecomm from "../../assets/images/ecomm.jpeg";
+import Quiz from "../../assets/images/quiz.png";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
 function ProjectsComp() {
@@ -47,6 +48,40 @@ function ProjectsComp() {
           </p>
         </div>
         <br></br>
+        <div className="image-container">
+          <img
+            className="image"
+            src={Quiz}
+            onClick={() => {
+              window.open("https://react---ecomm.web.app/");
+            }}
+            alt="Quiz app"
+          />
+          <footer
+            style={{ marginLeft: "auto", marginRight: "auto", padding: "1rem" }}
+          >
+            Click on image ⬆️
+            <span>
+              <strong style={{ marginLeft: "1rem", color: "black" }}>
+                {" "}
+                Project Repo:
+              </strong>
+              <a
+                style={{ color: "black" }}
+                href="https://github.com/T00lio/quiz"
+              >
+                <GitHubIcon />
+              </a>
+            </span>
+          </footer>
+          <p className="projects-text">
+            <strong>Quiz App:</strong> (in development) a full stack app made
+            using vite, node.js, express, postgresSQL, Sequelize (ORM) and
+            react. The app is a quiz app that allows users take quizzes and keep
+            up with their progress in know of web dev topics. The app is
+            currently in development and will be deployed soon.
+          </p>
+        </div>
 
         <br></br>
       </section>
