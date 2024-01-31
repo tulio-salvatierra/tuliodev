6a880b18be7a40248d36c29b94517792cb0b25dc
tuliosalvatierra <salvacorp@gmail.com>
2024-01-31 17:15:21 -0600

    Update Contact component styling and alignment

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,7 +10,7 @@ import emailjs from "@emailjs/browser";
 const styles = {
   container: {
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "left",
     alignItems: "center",
     marginTop: 100,
     marginBottom: 100,
@@ -41,6 +41,7 @@ const styles = {
   },
   textField: {
     width: "100%",
+    marginBottom: 20,
   },
 };
 
@@ -67,71 +68,58 @@ export default function Contact() {
   };
 
   return (
-    <section className="contact-content" id="contact">
-      <div className="sep"></div>
+    <>
       <h1 className="contact-text-image-mask">Contact me</h1>
-      <div style={styles.container}>
-        <Card style={styles.card}>
-          <CardContent>
-            <h3>Send me an email, let's talk!</h3>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <h3>Send me an email, let's talk!</h3>
 
-            <form ref={formRef} onSubmit={sendEmail}>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    type="text"
-                    id="first-name"
-                    label="First Name"
-                    variant="outlined"
-                    name="first-name"
-                    autoComplete="true"
-                    style={styles.textField}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    type="text"
-                    id="last-name"
-                    label="Last Name"
-                    variant="outlined"
-                    name="last-name"
-                    autoComplete="true"
-                    style={styles.textField}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    id="email"
-                    label="Email"
-                    name="email"
-                    variant="outlined"
-                    autoComplete="true"
-                    style={styles.textField}
-                    type="email"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    id="message"
-                    label="Message"
-                    variant="outlined"
-                    multiline
-                    rows={4}
-                    name="message"
-                    autoComplete="false"
-                    style={styles.textField}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button outline size="lg" type="submit">
-                    Send message
-                  </Button>
-                </Grid>
-              </Grid>
-            </form>
-          </CardContent>
-        </Card>
+        <form ref={formRef} onSubmit={sendEmail}>
+          <TextField
+            type="text"
+            id="first-name"
+            label="First Name"
+            variant="outlined"
+            name="first-name"
+            autoComplete="true"
+            style={styles.textField}
+          />
+
+          <TextField
+            type="text"
+            id="last-name"
+            label="Last Name"
+            variant="outlined"
+            name="last-name"
+            autoComplete="true"
+            style={styles.textField}
+          />
+
+          <TextField
+            id="email"
+            label="Email"
+            name="email"
+            variant="outlined"
+            autoComplete="true"
+            style={styles.textField}
+            type="email"
+          />
+
+          <TextField
+            id="message"
+            label="Message"
+            variant="outlined"
+            multiline
+            rows={4}
+            name="message"
+            autoComplete="false"
+            style={styles.textField}
+          />
+
+          <Button outline size="lg" type="submit">
+            Send message
+          </Button>
+        </form>
       </div>
-    </section>
+    </>
   );
 }
