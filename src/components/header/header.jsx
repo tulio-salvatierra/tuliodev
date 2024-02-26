3f420f2247aaf65c9d215a8297050dd327e189e1
tuliosalvatierra <salvacorp@gmail.com>
2024-02-25 22:01:05 -0600

    Update header component styles and import statement

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./header.css";
 import GitHubIcon from "@mui/icons-material/GitHub";
