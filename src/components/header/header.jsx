2167dc0bbff05b800becfcc8d51df0de08637a4a
tuliosalvatierra <salvacorp@gmail.com>
2024-04-06 15:21:01 -0500

    Update header styling and add social media icons

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -11,52 +11,54 @@ import Logo2 from "../../assets/images/_logo.svg";
 function Header() {
   return (
     <header className="header">
-      <Navbar light expand="lg" style={{ backgroundColor: "#f5f4f7" }}>
-        <NavbarBrand href="/" style={{ fontSize: "2rem", color: "#6f6f6f" }}>
-          <img src={Logo2} alt="headerLogo" height={20}></img>
-        </NavbarBrand>
+      <div className="container">
+        <Navbar light expand="lg" style={{ maxWidth: "1200px" }}>
+          <NavbarBrand href="/" style={{ fontSize: "2rem", color: "#6f6f6f" }}>
+            <img src={Logo2} alt="headerLogo" height={20}></img>
+          </NavbarBrand>
 
-        <nav className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="list">
-            <li className="listItem">
-              <a href="#projects">Projects</a>
-            </li>
-            <li className="listItem">
-              <a href="#experience">Experience</a>
-            </li>
-            <li>
-              <a href="#tech">Technologies</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-          </ul>
-        </nav>
+          <nav className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="list">
+              <li className="listItem">
+                <a href="#projects">Projects</a>
+              </li>
+              <li className="listItem">
+                <a href="#experience">Experience</a>
+              </li>
+              <li>
+                <a href="#tech">Technologies</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+            </ul>
+          </nav>
 
-        <div className="social-icons">
-          <a href="https://www.linkedin.com/in/tulio-salvatierra/">
-            <LinkedInIcon sx={{ fontSize: 30, color: "#6f6f6f" }} />
-          </a>
+          <div className="social-icons">
+            <a href="https://www.linkedin.com/in/tulio-salvatierra/">
+              <LinkedInIcon sx={{ fontSize: 30, color: "#6f6f6f" }} />
+            </a>
 
-          <a href="https://www.github.com/T00lio">
-            <GitHubIcon sx={{ fontSize: 30, color: "#6f6f6f" }} />
-          </a>
-          <a href="https://wa.me/17863146121">
-            <WhatsAppIcon sx={{ fontSize: 30, color: "#6f6f6f" }} />
-          </a>
-        </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-      </Navbar>
+            <a href="https://www.github.com/T00lio">
+              <GitHubIcon sx={{ fontSize: 30, color: "#6f6f6f" }} />
+            </a>
+            <a href="https://wa.me/17863146121">
+              <WhatsAppIcon sx={{ fontSize: 30, color: "#6f6f6f" }} />
+            </a>
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </Navbar>
+      </div>
     </header>
   );
 }
