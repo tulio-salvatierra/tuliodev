f8ade056fcac022088f3a72660ddba359da2644a
Tulio Salvatierra <salvacorp@gmail.com>
2024-10-31 20:48:01 -0500

    added panel animation to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,6 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Hero from "../components/Hero";
 import ExperienceComponent from "../components/experience/index";
 import Contact from "../components/Contact";
@@ -7,16 +9,55 @@ import CTA from "../components/CTA";
 import Skills from "../components/Skills/Skills";
 
 function HomePage() {
+  const panelRef = useRef(null);
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    var panels = gsap.utils.toArray(".panel");
+
+    panels.forEach((panel, i) => {
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: panel,
+          start: "bottom bottom",
+          pinSpacing: false,
+          pin: true,
+          scrub: true,
+
+          onRefresh: () =>
+            gsap.set(panel, {
+              transformOrigin:
+                "center " +
+                (panel.offsetHeight - window.innerHeight / 2) +
+                "px",
+            }),
+        },
+      });
+
+      tl.fromTo(
+        panel,
+        1,
+        { y: 0, rotate: 0, scale: 1, opacity: 1 },
+        { y: 0, rotateX: 0, scale: 0.5, opacity: 0.005 },
+        0
+      ).to(panel, 0.1, { opacity: 0 });
+    });
+  });
   return (
     <main id="home">
-      <Hero />
-      <div className="white-bg">
-        <Projects />
-        <CTA />
-        <ExperienceComponent />
-        <Skills />
+      <div className="panel-wrapper">
+        <div className="panel" ref={panelRef}>
+          <Hero />
+        </div>{" "}
+        <div className="white-bg panel" useRef={panelRef}>
+          <Projects />
+          <CTA />
+          <ExperienceComponent />
+          <Skills />
+
+          <Contact />
+        </div>
       </div>
-      <Contact />
     </main>
   );
 }
