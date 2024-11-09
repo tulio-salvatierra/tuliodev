d386a29ba56af5ff4ed789907583db01f8f1c022
Tulio Salvatierra <salvacorp@gmail.com>
2024-11-09 11:13:55 -0600

    heade animation implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./components/header";
 import Footer from "./components/Footer";
@@ -6,14 +7,36 @@ import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
 import AboutPage from "./pages/AboutPage";
 import Contact from "./components/Contact";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+  useEffect(() => {
+    const showHeader = gsap
+      .from(".top", {
+        yPercent: -100,
+        paused: true,
+        duration: 0.2,
+      })
+      .progress(1);
+
+    ScrollTrigger.create({
+      start: "top top",
+      end: "bottom bottom",
+      onUpdate: (self) => {
+        self.direction === -1 ? showHeader.play() : showHeader.reverse();
+      },
+    });
+  }, []);
+
   return (
     <>
-      <div className="top">
+      <div className="top z-3">
         <Header />
-      </div>{" "}
-      <main className="content">
+      </div>
+      <main className="content z--1000">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/#projects" element={<ProjectsPage />} />
