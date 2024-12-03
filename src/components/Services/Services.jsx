7b0d99c509bf9fdb7f6e9319340cf00a8b8e0f72
Tulio Salvatierra <salvacorp@gmail.com>
2024-12-02 22:10:29 -0600

    added animation to services section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,14 +1,38 @@
+import { useRef, useEffect } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
 import { servicesData } from "./ServicesData";
 import Arrow from "../../assets/icons/arrow.svg";
 import "./Services.css";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function ServicesComp() {
+  const eased = useRef([]);
+
+  useEffect(() => {
+    gsap.from(eased.current, {
+      y: 50, // Start position (below)
+      opacity: 0, // Start fully transparent
+      duration: 1, // Animation duration
+      ease: "power2.out", // Smooth easing
+      stagger: 0.3, // Stagger animations for a cascading effect
+      scrollTrigger: {
+        trigger: eased.current, // Trigger animation when elements come into view
+        start: "top 80%", // Start animation when top of element is 80% of viewport height
+        end: "bottom 20%", // Animation ends when bottom of element is 20% of viewport height
+        scrub: false, // Set to true for a smoother experience when scrolling
+      },
+    });
+  }, []);
   return (
     <section id="projects" className="project-section">
       <div className="project-header">
-        <h2 className="section-title">Services</h2>
+        <h2 className="section-title" ref={(el) => (eased.current[0] = el)}>
+          Services
+        </h2>
         <div className="d-flex subtitle-box">
-          <h3 className="welcome">
+          <h3 className="welcome" ref={(el) => (eased.current[1] = el)}>
             I specialize in delivering tailored solutions to elevate your
             digital presence. Whether you need a stunning website, impactful
             marketing strategies, or eye-catching animations, I’ve got you
@@ -18,7 +42,7 @@ export default function ServicesComp() {
           <img
             src={Arrow}
             alt="web development small businesses in Chicago"
-            className=""
+            ref={(el) => (eased.current[2] = el)}
           ></img>
         </div>
       </div>
