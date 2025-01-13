1aafab898b5b0206fce0cfe9fc37d46af8909e63
Tulio Salvatierra <salvacorp@gmail.com>
2025-01-12 22:12:29 -0600

    refactor(Contact): update layout to use grid for better structure; replace static icons with CustomButton components

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,37 +1,28 @@
 import { EMAIL, TELEPHONE } from "../../Constants";
-import PhoneIcon from "./../../assets/icons/contact/phone.svg";
-import EmailIcon from "./../../assets/icons/contact/mail.svg";
+import CustomButton from "../CustomButtom";
+import owl from "../../assets/images/contact/owl.png";
 import "./Contact.css";
 
 export default function Contact() {
   return (
-    <section className="contact-section h-100" id="contact">
+    <section className="contact-section" id="contact">
       <h1 className="contact-title">Contact</h1>
-      <h4 className="contact-text">
-        You can get in touch with me through the form below. I'll be happy to
-        discuss your project or answer any questions about freelancing services,
-        don't hesitate to reach out!
-      </h4>
-      <div className="contact-parent h-50 m-3 m-sm-0">
+
+      <div className="contact-parent">
+        {" "}
+        {/* Text and Buttons on the Left */}
         <div className="contact-info">
-          <div className="contact-item">
-            <img
-              src={EmailIcon}
-              alt="email icon for web develoment services in chicago"
-            ></img>
-            <a href={`mailto:${EMAIL}`} className="contact-text">
-              <h4>{EMAIL}</h4>
-            </a>
-          </div>
-          <div className="contact-item ju">
-            <img
-              src={PhoneIcon}
-              alt="phone icon for web develoment services in chicago"
-            ></img>
-            <a href={`tel:${TELEPHONE}`} className="contact-text">
-              <h4>{TELEPHONE}</h4>
-            </a>
-          </div>
+          <h4 className="contact-text">
+            You can get in touch with me through the form below. I'll be happy
+            to discuss your project or answer any questions about freelancing
+            services, don't hesitate to reach out!
+          </h4>
+          <CustomButton name="Email" link={`mailto:${EMAIL}`} />
+          <CustomButton name="Phone" link={`tel:${TELEPHONE}`} />
+        </div>
+        {/* Picture on the Right */}
+        <div className="contact-picture">
+          <img src={owl} alt="Contact" />
         </div>
       </div>
     </section>
