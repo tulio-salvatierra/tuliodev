93ad74e04a9fd97f0aa8205e47ddedee0fff2884
Tulio Salvatierra <salvacorp@gmail.com>
2025-03-15 16:41:48 -0500

    style(Hero, Footer, App): refactor CSS for improved layout and remove unused styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -23,11 +23,11 @@ function Hero() {
     const easeFromBelow = gsap.matchMedia();
     easeFromBelow.add("(min-width: 801px)", () => {
       gsap.from(panelsBottomRef.current, {
-        y: 25,
+        y: 35,
         opacity: 0,
-        duration: 1,
+        duration: 1.2,
         ease: "power4.out",
-        stagger: 0.4,
+        stagger: 0.6,
         scrollTrigger: {
           trigger: panelsBottomRef.current,
           start: "top 80%",
@@ -35,103 +35,120 @@ function Hero() {
           scrub: false,
         },
       });
-    });   
+    });
   }, []);
   return (
     <>
-      <section className="hero-section" id="hero">
-        <HeroSub />
-        <h1
-          className="hero-title"
-          ref={(el) => (panelsBottomRef.current[0] = el)}
-        >
-          TULIO SALVATIERRA
-        </h1>
-
-        <div className="hero flex pt-5">
-          <div
-            className="col1 d-flex flex-column align-content-start justify-content-start"
-            ref={(el) => (panelsBottomRef.current[1] = el)}
-          >
-            <h3 className="hero-welcome">
-              I help elevate your online presence and drive business growth by
-              delivering responsive, user-friendly interfaces and seamless user
-              experience.
-            </h3>
-            <CustomButton
-              name="Start my Project Today"
-              url={`tel:${TELEPHONE}`}
-            />
+      <section
+        className="hero-section"
+        id="hero"
+        ref={(el) => (panelsBottomRef.current[0] = el)}
+      >
+        <div className="hero justify-content-between">
+          <div className="div-2 row-12">
+            <h1
+              className="name col-8"
+              ref={(el) => (panelsBottomRef.current[1] = el)}
+            >
+              TULIO
+            </h1>
+            <h1
+              className="last-name col-1"
+              ref={(el) => (panelsBottomRef.current[2] = el)}
+            >
+              SALVATIERRA
+            </h1>
           </div>
-          <div className="col2" ref={(el) => (panelsBottomRef.current[2] = el)}>
+          <div className="row">
+            <HeroSub />
+          </div>
+          <div
+            className="row"
+            ref={(el) => (panelsBottomRef.current[4] = el)}
+          >
             <img
               src={profile_pic}
               alt="tulio_salvatierra"
               className="hero-image"
             />
-          </div>
+          
           <div
-            className="col3 d-flex flex-column align-content-between justify-content-between"
+            className="div-3 col-4"
             ref={(el) => (panelsBottomRef.current[3] = el)}
           >
-            <div className="hero-icons justify-content-between d-none d-sm-flex">
-              <a className="fs-4 text-decoration-none" href={LINKEDIN_URL}>
-                LINKEDIN
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 032 32"
-                >
-                  <path
-                    fill="none"
-                    stroke="#e3e4e2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.5"
-                    d="M6 19L19 6m0 0v12.48M19 6H6.52"
-                  />
-                </svg>
-              </a>
-
-              <a className="fs-4 text-decoration-none" href={GITHUB_URL}>
-                GITHUB
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 032 32"
-                >
-                  <path
-                    fill="none"
-                    stroke="#e3e4e2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.5"
-                    d="M6 19L19 6m0 0v12.48M19 6H6.52"
-                  />
-                </svg>
-              </a>
-              <a className="fs-4 text-decoration-none" href={UPWORK_URL}>
-                UPWORK
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 032 32"
-                >
-                  <path
-                    fill="none"
-                    stroke="#e3e4e2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="1.5"
-                    d="M6 19L19 6m0 0v12.48M19 6H6.52"
-                  />
-                </svg>
-              </a>
-            </div>
+            <h2 className="">FULL STACK DEVELOPER</h2>
+            <p className="">
+              I help elevate your online presence and drive business growth by
+              delivering responsive, user-friendly interfaces and seamless user
+              experience.
+            </p>
+            <CustomButton
+              name="Start my Project Today"
+              url={`tel:${TELEPHONE}`}
+            />
           </div>
+
+          <div
+            className="col-4 links"
+            ref={(el) => (panelsBottomRef.current[5] = el)}
+          >
+            <a className="fs-4 text-decoration-none" href={LINKEDIN_URL}>
+              LINKEDIN
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 032 32"
+              >
+                <path
+                  fill="none"
+                  stroke="#e3e4e2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  d="M6 19L19 6m0 0v12.48M19 6H6.52"
+                />
+              </svg>
+            </a>
+
+            <a className="fs-4 text-decoration-none" href={GITHUB_URL}>
+              GITHUB
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 032 32"
+              >
+                <path
+                  fill="none"
+                  stroke="#e3e4e2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  d="M6 19L19 6m0 0v12.48M19 6H6.52"
+                />
+              </svg>
+            </a>
+            <a className="fs-4 text-decoration-none" href={UPWORK_URL}>
+              UPWORK
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 032 32"
+              >
+                <path
+                  fill="none"
+                  stroke="#e3e4e2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="1.5"
+                  d="M6 19L19 6m0 0v12.48M19 6H6.52"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
         </div>
       </section>
     </>
