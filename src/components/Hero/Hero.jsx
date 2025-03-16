fdfda9e615353f838482b26a80b60dd4f9f59958
Tulio Salvatierra <salvacorp@gmail.com>
2025-03-16 11:45:01 -0500

    style(Hero, App): update styles for improved readability and layout; refactor Hero component structure

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -45,110 +45,103 @@ function Hero() {
         ref={(el) => (panelsBottomRef.current[0] = el)}
       >
         <div className="hero justify-content-between">
-          <div className="div-2 row-12">
+          <div className="div-2 row flex">
             <h1
-              className="name col-8"
+              className="name"
               ref={(el) => (panelsBottomRef.current[1] = el)}
             >
               TULIO
             </h1>
             <h1
-              className="last-name col-1"
+              className="last-name"
               ref={(el) => (panelsBottomRef.current[2] = el)}
             >
               SALVATIERRA
             </h1>
-          </div>
-          <div className="row">
-            <HeroSub />
-          </div>
-          <div
-            className="row"
-            ref={(el) => (panelsBottomRef.current[4] = el)}
-          >
             <img
               src={profile_pic}
               alt="tulio_salvatierra"
               className="hero-image"
             />
-          
-          <div
-            className="div-3 col-4"
-            ref={(el) => (panelsBottomRef.current[3] = el)}
-          >
-            <h2 className="">FULL STACK DEVELOPER</h2>
-            <p className="">
-              I help elevate your online presence and drive business growth by
-              delivering responsive, user-friendly interfaces and seamless user
-              experience.
-            </p>
-            <CustomButton
-              name="Start my Project Today"
-              url={`tel:${TELEPHONE}`}
-            />
           </div>
 
-          <div
-            className="col-4 links"
-            ref={(el) => (panelsBottomRef.current[5] = el)}
-          >
-            <a className="fs-4 text-decoration-none" href={LINKEDIN_URL}>
-              LINKEDIN
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 032 32"
-              >
-                <path
-                  fill="none"
-                  stroke="#e3e4e2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="1.5"
-                  d="M6 19L19 6m0 0v12.48M19 6H6.52"
-                />
-              </svg>
-            </a>
+          <HeroSub />
 
-            <a className="fs-4 text-decoration-none" href={GITHUB_URL}>
-              GITHUB
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 032 32"
-              >
-                <path
-                  fill="none"
-                  stroke="#e3e4e2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="1.5"
-                  d="M6 19L19 6m0 0v12.48M19 6H6.52"
-                />
-              </svg>
-            </a>
-            <a className="fs-4 text-decoration-none" href={UPWORK_URL}>
-              UPWORK
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                viewBox="0 032 32"
-              >
-                <path
-                  fill="none"
-                  stroke="#e3e4e2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="1.5"
-                  d="M6 19L19 6m0 0v12.48M19 6H6.52"
-                />
-              </svg>
-            </a>
+          <div
+            className="row justify-content-between"
+            ref={(el) => (panelsBottomRef.current[4] = el)}
+          >
+            <div
+              className="right"
+              ref={(el) => (panelsBottomRef.current[3] = el)}
+            >
+              <h2>FULL STACK DEVELOPER</h2>
+              <p className="">
+                I help elevate your online presence and drive business growth by
+                delivering responsive, user-friendly interfaces and seamless
+                user experience.
+              </p>
+              <CustomButton name="Let's talk!" url={`tel:${TELEPHONE}`} />
+            </div>
+
+            <div className="links">
+              <a className="fs-5 text-decoration-none" href={LINKEDIN_URL}>
+                LINKEDIN
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 032 32"
+                >
+                  <path
+                    fill="none"
+                    stroke="#e3e4e2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    d="M6 19L19 6m0 0v12.48M19 6H6.52"
+                  />
+                </svg>
+              </a>
+
+              <a className="fs-5 text-decoration-none" href={GITHUB_URL}>
+                GITHUB
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 032 32"
+                >
+                  <path
+                    fill="none"
+                    stroke="#e3e4e2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    d="M6 19L19 6m0 0v12.48M19 6H6.52"
+                  />
+                </svg>
+              </a>
+              <a className="fs-5 text-decoration-none" href={UPWORK_URL}>
+                UPWORK
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 032 32"
+                >
+                  <path
+                    fill="none"
+                    stroke="#e3e4e2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    d="M6 19L19 6m0 0v12.48M19 6H6.52"
+                  />
+                </svg>
+              </a>
+            </div>
           </div>
-        </div>
         </div>
       </section>
     </>
